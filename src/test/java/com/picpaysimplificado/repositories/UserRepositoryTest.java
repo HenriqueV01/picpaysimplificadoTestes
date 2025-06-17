96645230a73212031aadc272a647aa92e1fdcc54
HenriqueV01 <henrique.unibratec01@gmail.com>
2025-06-17 10:57:08 -0300

    Implementacao do teste: findUserByDocumentCase1

--- src/test/java/com/picpaysimplificado/repositories/UserRepositoryTest.java
+++ src/test/java/com/picpaysimplificado/repositories/UserRepositoryTest.java
@@ -1,15 +1,46 @@
 package com.picpaysimplificado.repositories;
 
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.dtos.UserDTO;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 @DataJpaTest
+@ActiveProfiles("test")
 class UserRepositoryTest {
 
+    @Autowired
+    EntityManager entityManager;
+
+    @Autowired
+    UserRepository userRepository;
+
     @Test
-    void findUserByDocument() {
-        //teste2
+    @DisplayName("Should get User successfully from DB")
+    void findUserByDocumentCase1() {
+        String document = "99999999901";
+        UserDTO data = new UserDTO("Henrique", "Venancio", document, new BigDecimal(10), "teste@email.com", "123", UserType.COMMON);
+        this.createUser(data);
+
+        Optional<User> result = this.userRepository.findUserByDocument(document);
+
+        assertThat(result.isPresent()).isTrue();
     }
+
+    private void createUser(UserDTO data){
+        User newUser = new User(data);
+        this.entityManager.persist(newUser);
+    }
+
+
 }
