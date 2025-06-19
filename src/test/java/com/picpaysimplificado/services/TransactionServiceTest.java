13076b95b07d20a0d3850f6dadba4af19e9ca742
HenriqueV01 <henrique.unibratec01@gmail.com>
2025-06-19 12:22:12 -0300

    feat: TransactionServiceTest

--- src/test/java/com/picpaysimplificado/services/TransactionServiceTest.java
+++ src/test/java/com/picpaysimplificado/services/TransactionServiceTest.java
@@ -1,12 +1,60 @@
 package com.picpaysimplificado.services;
 
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.repositories.TransactionRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
 
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class TransactionServiceTest {
 
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private TransactionRepository repository;
+
+    @Mock
+    private AuthorizationService authService;
+
+    @Mock
+    private NotificationService notificationService;
+
+    @Autowired
+    @InjectMocks
+    private TransactionService transactionService;
+
+    /* Subistituido por @ExtendWith(MockitoExtension.class).
+    @BeforeEach
+    void setup(){
+        MockitoAnnotations.initMocks(this);
+    }*/
+
     @Test
-    void createTransaction() {
+    @DisplayName("Should create transaction successfully when everything is OK")
+    void createTransactionCase1() throws Exception {
+        User sender = new User(1L, "Maria", "Souza", "99999999901", "maria@email.com", "123", new BigDecimal(10), UserType.COMMON);
+        User receiver = new User(2L, "Joao", "Silva", "999999999902", "joao@email.com", "123", new BigDecimal(10), UserType.COMMON);
+
+        when(userService.findUserById(1L)).thenReturn(sender);
+        when(userService.findUserById(2L)).thenReturn(receiver);
     }
+
+    @Test
+    @DisplayName("Should throw exception when transaction is not allowed")
+    void createTransactionCase2() {
+    }
+
+
+
 }
