72512bad2edb66a2f3c41d9080a973c170aa1ec3
HenriqueV01 <henrique.unibratec01@gmail.com>
2025-06-20 09:45:32 -0300

    feat: createTransactionCase1

--- src/test/java/com/picpaysimplificado/services/TransactionServiceTest.java
+++ src/test/java/com/picpaysimplificado/services/TransactionServiceTest.java
@@ -2,6 +2,7 @@ package com.picpaysimplificado.services;
 
 import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.dtos.TransactionDTO;
 import com.picpaysimplificado.repositories.TransactionRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,7 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TransactionServiceTest {
@@ -48,6 +50,23 @@ class TransactionServiceTest {
 
         when(userService.findUserById(1L)).thenReturn(sender);
         when(userService.findUserById(2L)).thenReturn(receiver);
+
+        when(authService.authorizeTransaction(any(), any())).thenReturn(true);
+
+        TransactionDTO request = new TransactionDTO(new BigDecimal(10), 1L, 2L);
+        transactionService.createTransaction(request);
+
+        verify(repository, times(1)).save(any());
+
+        sender.setBalance(new BigDecimal(0));
+        verify(userService, times(1)).saveUser(sender);
+
+        receiver.setBalance(new BigDecimal(20));
+        verify(userService, times(1)).saveUser(receiver);
+
+        verify(notificationService, times(1)).sendNotification(sender, "Transação realizada com sucesso");
+        verify(notificationService, times(1)).sendNotification(receiver, "Transação recebida com sucesso");
+
     }
 
     @Test
